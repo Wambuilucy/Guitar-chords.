ea358e810bb8bd36f6bd4b32446cf8ccb4599ef8
Wambuilucy <wambuilucie99@gmail.com>
2022-03-30 12:13:00 +0300

    testing

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 - [ ] Add Unit Tests
   - Test for Ed Sheeran - Perfect
   - Test for Jason Mraz - I'm Yours (because of the title parsing)
-  
+  - Test for boyce avenue - acoustic cover # I need more (Guitar)lessons on acoustic sounds. 
   
   
   
